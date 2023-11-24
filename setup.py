ff7259c822fcf6cc13ddc713ccfa4a3d1355a3ec
suiwan <1054699517@qq.com>
2023-11-24 09:51:59 +0800

    add require pysimplegui

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
         "flask-cors",
         "flask-socketio",
         "MMEdu>=0.1.21",
-        "BaseNN>=0.2.2"
+        "BaseNN>=0.2.2",
+        "pysimplegui"
     ],
     python_requires='>=3.8',
     license="MIT",
